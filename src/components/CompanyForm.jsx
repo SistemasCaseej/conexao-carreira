c9aaac7b0a66e2e4ec5807208790849a80df47a3
Jose-silvaa <jvitorfacanha@gmail.com>
2025-08-07 22:57:39 -0300

    86aa8e00t

--- src/components/CompanyForm.jsx
+++ src/components/CompanyForm.jsx
@@ -1,30 +1,53 @@
 'use client'
 
 import { Dialog, DialogContent, DialogTitle, DialogTrigger, DialogHeader, DialogDescription } from "@/components/ui/dialog";
-import { CirclePlus } from "lucide-react";
 import {Button} from "@/components/ui/button";
 import {IconPlus} from "@tabler/icons-react";
 import {GenericForm} from "@/components/Form";
+import {toast} from "sonner";
+import {useRouter} from "next/navigation";
 
 
 
 export default function CompanyForm(){
 
+    const router = useRouter();
+
+    const handleSubmit = async (data) => {
+
+        try{
+            const response = await fetch('/api/company', {
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(data),
+            })
+
+            const responseData = await response.json();
+
+            if(!response.ok) {
+                toast.error(responseData.message || "Erro ao enviar formulário.");
+            }else {
+                toast.error(responseData.message || "Administrador cadastrado com sucesso!");
+                setOpen(false);
+                router.refresh();
+            }
+        }catch (error){
+            console.error(error)
+        }
+
+    };
     return (
         <Dialog>
             <form>
                 <DialogTrigger asChild>
-                    <Button variant="outline" size="sm" className="ml-2 border">
+                    <Button variant="outline" size="sm" className="ml-2 border cursor-pointer">
                         <IconPlus />
-                        <span className="hidden lg:inline">Adicionar Usuário</span>
+                        <span className="hidden lg:inline">Adicionar Empresa</span>
                     </Button>
                 </DialogTrigger>
                 <DialogContent className="sm:min-w-[670px]">
                     <DialogHeader>
-                        <DialogTitle>Cadastrar Empresa</DialogTitle>
-                        <DialogDescription className="">
-                            Inclua a adição da empresa. Clique em salvar quando terminar.
-                        </DialogDescription>
+                        <DialogTitle className="font-semibold text-black">CADASTRAR EMPRESA</DialogTitle>
                     </DialogHeader>
                     <GenericForm
                         fields={[
@@ -32,10 +55,9 @@ export default function CompanyForm(){
                             {label: "Nome Fantasia", name: "nome"},
                             {label: "CNPJ", name: "cnpj"},
                             {label: "Porte da Empresa", name: "porte"},
-                            {label: "Ramo de Atuação", name: "ramo"},
-                            {label: "WebSite", name: "site"},
-                            {label: "Cidade", name: "city"},
                         ]}
+                        logo={true}
+                        onSubmit={handleSubmit}
                     />
                 </DialogContent>
             </form>
